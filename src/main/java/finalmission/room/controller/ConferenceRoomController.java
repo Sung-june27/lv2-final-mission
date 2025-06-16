c53c98c6b691c5ff3379085086d7f2cbf0f97e71
sung-june27 <kmymy27@gmail.com>
2025-06-16 23:56:33 +0900

    feat: ConferenceRoom 생성 구현

---
+++ src/main/java/finalmission/room/controller/ConferenceRoomController.java
@@ -0,0 +1,28 @@
+package finalmission.room.controller;
+
+import finalmission.room.dto.request.CreateRoomRequest;
+import finalmission.room.dto.response.CreateRoomResponse;
+import finalmission.room.service.ConferenceRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/room")
+public class ConferenceRoomController {
+
+    private final ConferenceRoomService conferenceRoomService;
+
+    @PostMapping
+    public ResponseEntity<CreateRoomResponse> createConferenceRoom(
+            @RequestBody CreateRoomRequest request
+    ) {
+        CreateRoomResponse response = conferenceRoomService.create(request);
+
+        return ResponseEntity.ok().body(response);
+    }
+}
--- src/main/java/finalmission/room/service/ConferenceRoomService.java
+++ src/main/java/finalmission/room/service/ConferenceRoomService.java
@@ -2,12 +2,15 @@ package finalmission.room.service;
 
 import finalmission.global.error.exception.NotFoundException;
 import finalmission.room.domain.ConferenceRoom;
+import finalmission.room.dto.request.CreateRoomRequest;
+import finalmission.room.dto.response.CreateRoomResponse;
 import finalmission.room.repository.ConferenceRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class ConferenceRoomService {
 
@@ -17,5 +20,12 @@ public class ConferenceRoomService {
     public ConferenceRoom getById(Long conferenceRoomId) {
         return conferenceRoomRepository.findById(conferenceRoomId)
                 .orElseThrow(() -> new NotFoundException("회의실을 찾을 수 없습니다."));
+    }
+
+    public CreateRoomResponse create(CreateRoomRequest request) {
+        ConferenceRoom conferenceRoom = request.toConferenceRoom();
+        ConferenceRoom saved = conferenceRoomRepository.save(conferenceRoom);
+
+        return CreateRoomResponse.from(saved);
     }
 }
