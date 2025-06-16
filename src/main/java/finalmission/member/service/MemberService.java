846f2fe3ca8dd10a018441c23debba7965e50a8a
sung-june27 <kmymy27@gmail.com>
2025-06-16 23:50:11 +0900

    feat: Member 생성 구현

--- src/main/java/finalmission/member/service/MemberService.java
+++ src/main/java/finalmission/member/service/MemberService.java
@@ -2,12 +2,15 @@ package finalmission.member.service;
 
 import finalmission.global.error.exception.NotFoundException;
 import finalmission.member.domain.Member;
+import finalmission.member.dto.request.CreateMemberRequest;
+import finalmission.member.dto.response.CreateMemberResponse;
 import finalmission.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class MemberService {
 
@@ -17,5 +20,12 @@ public class MemberService {
     public Member getById(Long memberId) {
         return memberRepository.findById(memberId)
                 .orElseThrow(() -> new NotFoundException("멤버를 찾을 수 없습니다."));
+    }
+
+    public CreateMemberResponse create(CreateMemberRequest request) {
+        Member member = request.toMember();
+        Member saved = memberRepository.save(member);
+
+        return CreateMemberResponse.from(saved);
     }
 }
